4ec287fdb4a9e8b6992bb7dd255315a0727efb38
Alexvtoriq <161458256+Alexvtoriq@users.noreply.github.com>
2025-11-18 20:07:31 +0200

    Add files via upload

---
+++ Zadacha 1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+    using System;
+
+    namespace TransportCalculator
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                
+                int n = int.Parse(Console.ReadLine());
+                string period = Console.ReadLine(); 
+
+              
+                var service = new TransportService();
+
+                var request = new TravelRequestModel
+                {
+                    Kilometers = n,
+                    Period = period
+                };
+
+                var result = service.GetCheapestOption(request);
+
+             
+                Console.WriteLine($"{result.CheapestPrice:F2}");
+            }
+        }
+    }
+
+
